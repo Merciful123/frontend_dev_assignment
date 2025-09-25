5fe995bad766964eb2ec87cd83c1a340245a2250
Aamir Raza <raza949597@gmail.com>
2025-09-26 01:02:17 +0530

    deploy error fix

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
   experimental: {
-    optimizeCss: true,
+    optimizeCss: false,
   },
   reactStrictMode: true,
 };
